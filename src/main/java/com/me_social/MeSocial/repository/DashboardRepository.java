54f43d69598f637615215b491963370c96384a01
Luc Pham <phamtienluc0601@gmail.com>
2024-10-17 21:53:24 +0700

    Fix little bug in repo

--- src/main/java/com/me_social/MeSocial/repository/DashboardRepository.java
+++ src/main/java/com/me_social/MeSocial/repository/DashboardRepository.java
@@ -1,10 +1,13 @@
 package com.me_social.MeSocial.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.me_social.MeSocial.entity.modal.User;
+
 @Repository
-public interface DashboardRepository {
+public interface DashboardRepository extends JpaRepository<User, Long>{
     @Query("""
             SELECT COUNT(c) FROM Comment c
             """)
